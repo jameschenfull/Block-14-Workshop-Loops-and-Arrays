3a506d29308e4d69bc8fbac53255e213a0b4fdbc
James Chen <126843156+jameschenfull@users.noreply.github.com>
2023-04-24 17:18:42 -0700

    Add files via upload

--- only-odd.js
+++ only-odd.js
@@ -4,7 +4,7 @@ let odd = Array.filter((num) => num % 2 !== 0);
 //Input Arrays	Expected Results
 //[2, 4, 6, 8, 11, 20, 15, 22]	[ 11, 15]
 let num = [2, 4, 6, 8, 11, 20, 15, 22];
-console.log;
+console.log(odd);
 
 //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	[1, 3, 5, 7, 9]
 //[70, 42, 55, 81, 21, 91, 34]	[55, 81, 21, 91]
